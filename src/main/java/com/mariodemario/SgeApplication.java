28b8a94dd7853ad51bae22fc63b903d82b95661f
Mario de Mario Nhanengue <mariopalmira7@gmail.com>
2026-07-23 13:50:04 +0200

    Estrutura do projecto

---
+++ src/main/java/com/mariodemario/SgeApplication.java
@@ -0,0 +1,13 @@
+package com.mariodemario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgeApplication.class, args);
+	}
+
+}
